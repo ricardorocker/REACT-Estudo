24a634d945f12080e8e07065826bdd79395ffebe
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-01 16:27:42 -0300

    Hello World e Estrutura do React

---
+++ 01_INTRO/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <h2>Emmet funcionou</h2>
+    </div>
+  );
+}
+
+export default App;

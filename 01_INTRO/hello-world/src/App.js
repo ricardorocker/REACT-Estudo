c3a5f84fc8bb4ab5cf53bc6ac02534cb414ef73c
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-01 16:43:11 -0300

    Exercício - Alterar html, css e title

--- 01_INTRO/hello-world/src/App.js
+++ 01_INTRO/hello-world/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World React!</h1>
-      <h2>Emmet funcionou</h2>
+      <h1>Estudando React</h1>
+      <h2>Com curso do Matheus Battisti</h2>
+      <p>Me chamo Ricardo Rocker, e até o final de 2023, 
+      serei desenvolvedor Senior!</p>
     </div>
   );
 }

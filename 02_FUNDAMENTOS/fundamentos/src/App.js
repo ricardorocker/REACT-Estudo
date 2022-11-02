a9924220697a87ddc5b6eadfb1a8d488eca2e41f
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-02 20:26:48 -0300

    Importação de Componentes

--- 02_FUNDAMENTOS/fundamentos/src/App.js
+++ 02_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,8 +1,14 @@
+// Components
+import FirstComponent from "./components/FirstComponent";
+
+// Styles / CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
     </div>
   );
 }

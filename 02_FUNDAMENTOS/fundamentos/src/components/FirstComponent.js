e8616c530b8899a084a7738f4f91e2cb610ee1da
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-02 20:38:50 -0300

    Comentários no componente

--- 02_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 02_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,14 @@
+//  arquivo de estilo
+
 const FirstComponent = () => {
+    // essa função faz isso
+
+    /*
+    multi line
+    */
     return (
         <div>
+            {/* Comentário dentro do elemento pai */}
             <h1>Meu primeiro compenente</h1>
             <p className="teste">
                 Teste de atributo class(className), pois class é classe de JS.

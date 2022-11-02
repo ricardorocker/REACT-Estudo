d0c913c98638b8d89cd8305552b812ee4af795b0
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-02 20:34:17 -0300

    Conhecendo o JSX

--- 02_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 02_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -2,8 +2,11 @@ const FirstComponent = () => {
     return (
         <div>
             <h1>Meu primeiro compenente</h1>
+            <p className="teste">
+                Teste de atributo class(className), pois class é classe de JS.
+            </p>
         </div>
-    )
+    );
 };
 
 export default FirstComponent;

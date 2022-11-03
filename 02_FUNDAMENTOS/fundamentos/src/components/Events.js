9a0cc171c983df21e145352da0eb34fbb5dac725
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-02 21:10:25 -0300

    Funções no evento

--- 02_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 02_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -9,7 +9,23 @@ const Events = () => {
     return (
         <div>
             <div>
-                <button onClick={handleMyEvent}>Clique Aqui!</button>
+                <button onClick={handleMyEvent}>
+                    Clique Aqui!
+                </button>
+            </div>
+            <div>
+                <button onClick={() => console.log("Função inline")}>
+                    Clique Aqui tambem!
+                </button>
+            </div>
+            <div>
+                <button onClick={() => {
+                    if (true){
+                        console.log("Isso não deveria existir, esta complicando o html desnecessariamente!")
+                    }
+                }}>
+                    Clique Aqui, por favor.
+                </button>
             </div>
         </div>
     );

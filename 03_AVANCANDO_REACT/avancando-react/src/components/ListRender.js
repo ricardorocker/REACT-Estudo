8934b33ce95ba0004ed88c49ac30211697e0e65b
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-05 16:50:38 -0300

    Propriedade Key

--- 03_AVANCANDO_REACT/avancando-react/src/components/ListRender.js
+++ 03_AVANCANDO_REACT/avancando-react/src/components/ListRender.js
@@ -1,16 +1,37 @@
 import { useState } from "react";
 
 const ListRender = () => {
+    // Sem chave unica
     const [list] = useState(["Snow", "Lannister", "Stark"]);
+
+    // Com chave unica
+    const [family] = useState([
+        { id: 1, name: "Snow", qtde: 2 },
+        { id: 32, name: "Lannister", qtde: 8 },
+        { id: 54631, name: "Stark", qtde: 5 },
+    ])
 
     return (
         <div>
             <h1>ListRender</h1>
             <ul>
+                {/* SEM PROPRIEDADE KEY
                 {list.map((sobrenome) => (
                     <li>{sobrenome}</li>
                 ))}
+                */}
+                {/* Index como key, não sendo chave unica */}
+                {list.map((sobrenome, i) => (
+                    <li key={i}>{sobrenome}</li>
+                ))}
+                {/* Usando chave unica */}
+                {family.map((family) => (
+                    <li key={family.id}>
+                        {family.name} - {family.qtde}
+                    </li>
+                ))}
             </ul>
+
         </div>
     )
 }

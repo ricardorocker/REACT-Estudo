256c67cc23c1b0b6cb16fa3d1b2063c4d99bd798
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-06 16:45:48 -0300

    Seção 4 - Css inline style dinâmico

--- 04_CSS_REACT/css-react/src/App.js
+++ 04_CSS_REACT/css-react/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import { useState } from "react";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Ricardo");
+
   return (
     <div className="App">
       {/* CSS global */}
@@ -13,6 +17,21 @@ function App() {
       <p style={{ color: "blue", padding: "25px", borderTop: "2px solid black" }}>
         Este elemento foi estilizado de forma inline
       </p>
+      {/* CSS inline dinâmico */}
+      <h3 style={n < 10 ? ({ color: "purple" }) : ({ color: "pink" })}>
+        CSS inline dinâmico
+      </h3>
+      <h3 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>
+        CSS inline dinâmico
+      </h3>
+      <h3 style={
+        name === "Ricardo" ? (
+          { color: "white", backgroundColor: "green" }
+        ) : (
+          null
+        )}>
+        CSS inline dinâmico
+      </h3>
     </div>
   );
 }

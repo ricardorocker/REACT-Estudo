4c976e06c17cde05fbe601bba1bf19766218db07
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-06 16:29:53 -0300

    Seção 4 - Css de Componentes

--- 04_CSS_REACT/css-react/src/App.js
+++ 04_CSS_REACT/css-react/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* Css global */}
       <h1>React com CSS</h1>
+      {/* Css de componente */}
+      <MyComponent />
+      <p>Este paragrafo é do App.js</p>
     </div>
   );
 }
---
+++ 04_CSS_REACT/css-react/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Css de componente</h1>
+        <p>Este é o parágrafo do componente</p>
+        <p className="my-comp-paragraph">Este tambem é do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent

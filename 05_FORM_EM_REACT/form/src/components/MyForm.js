655a73f30fc664280ef51f587d308d42a7317382
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-08 15:34:17 -0300

    Gerenciamento de dados de Inputs

--- 05_FORM_EM_REACT/form/src/components/MyForm.js
+++ 05_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,18 +1,31 @@
 import './MyForm.css';
 
+import { useState } from 'react';
+
 const MyForm = () => {
+    // 3 - Gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log("Name (e.target.value)", name);
+
     return (
         <div>
             {/* 1 - Criação de Form */}
             <form>
                 <div>
                     <label htmlFor="name">Nome: </label>
-                    <input type="text" name="name" placeholder="Digite o seu nome" />
+                    <input type="text" name="name" placeholder="Digite o seu nome" 
+                    onChange={handleName} />
                 </div>
                 {/* 2 - Label envolvendo Input */}
                 <label>
                     <span>Email:</span>
-                    <input type="text" name="name" placeholder="Digite o seu email" />
+                    <input type="text" name="email" placeholder="Digite o seu email" />
                 </label>
                 <input type="submit" value="Enviar" />
             </form>

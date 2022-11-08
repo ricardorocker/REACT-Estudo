00c8e63ffea1d76c130c542fb464f5e631c631c7
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-08 18:04:09 -0300

    Criando componentes do projeto

---
+++ 06_SECRET_WORD/secretword/src/components/Game.js
@@ -0,0 +1,10 @@
+import './Game.css'
+
+const Game = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Game;
---
+++ 06_SECRET_WORD/secretword/src/components/GameOver.js
@@ -0,0 +1,10 @@
+import './GameOver.css'
+
+const GameOver = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default GameOver;

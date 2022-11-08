b1e1994bfe9d0c6fc626b00daee7a79d8403b6aa
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-08 14:41:46 -0300

    Criando Formulários

---
+++ 05_FORM_EM_REACT/form/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FORMS</h1>    
+      <MyForm/>
+    </div>
+  );
+}
+
+export default App;

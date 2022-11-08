e8591f575c8d3ce7cb09319e2e81b4e6ae41ca54
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-08 14:57:20 -0300

    Label envolvendo Input

--- 05_FORM_EM_REACT/form/src/components/MyForm.js
+++ 05_FORM_EM_REACT/form/src/components/MyForm.js
@@ -9,6 +9,11 @@ const MyForm = () => {
                     <label htmlFor="name">Nome: </label>
                     <input type="text" name="name" placeholder="Digite o seu nome" />
                 </div>
+                {/* 2 - Label envolvendo Input */}
+                <label>
+                    <span>Email:</span>
+                    <input type="text" name="name" placeholder="Digite o seu email" />
+                </label>
                 <input type="submit" value="Enviar" />
             </form>
         </div>

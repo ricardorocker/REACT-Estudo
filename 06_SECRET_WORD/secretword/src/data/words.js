f86f54c59adb5daf08f994aae3001505bea49000
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-08 17:20:22 -0300

    Criando o projeto

---
+++ 06_SECRET_WORD/secretword/src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Pneu"],
+    fruta: ["Maçã", "Mamão", "Melancia"],
+    país: ["Brasil", "Alemanha", "Grécia"],
+    sentimento: ["Alegria", "Amor", "Felicidade"],
+    programacao: ["Angular", "React", "HTML"],
+}

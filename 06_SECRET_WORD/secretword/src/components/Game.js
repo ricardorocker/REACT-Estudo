00f5991f7319c8bb4f0633415276b56f16229d6c
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-09 16:27:18 -0300

    Escolhendo palavra e categoria de forma aleatória

--- 06_SECRET_WORD/secretword/src/components/Game.js
+++ 06_SECRET_WORD/secretword/src/components/Game.js
@@ -4,6 +4,10 @@ const Game = ({ verifyLetter }) => {
     return (
         <div>
             <h1>Game</h1>
+            <p>A Dica é: </p>
+            <h3>{"pickedCategory"}</h3>
+            <p>A palavra é: </p>
+            <h3>{"pickedWord"}</h3>
             <button onClick={verifyLetter}>Finalizar jogo</button>
         </div>
     )

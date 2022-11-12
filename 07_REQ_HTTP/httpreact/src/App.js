2ce09b3234727ea879f5eca46f06c3c25c7cee62
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-12 00:58:40 -0300

    Adicionando dados

--- 07_REQ_HTTP/httpreact/src/App.js
+++ 07_REQ_HTTP/httpreact/src/App.js
@@ -5,8 +5,10 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-  // RESGATANDO DADOS
+  // 1 - RESGATANDO DADOS
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url);
@@ -24,12 +26,57 @@ function App() {
     fetchData();
   }, []);
 
+  // 2 - ADIÇÃO DE PRODUTOS
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newProduct = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct)
+    })
+
+    console.log("res", res);
+    console.log("newProduct", newProduct);
+    console.log("JSON.stringify(newProduct)", JSON.stringify(newProduct));
+  };
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
       {products.map((product) => {
-        return <li key={product.id}>{product.name} - {product.price}</li>
+        return <li key={product.id}>{product.name} - R${product.price}</li>
       })}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Descrição
+          </label>
+          <input
+            type="text"
+            name='name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>
+            Preço
+          </label>
+          <input
+            type="number"
+            name='price'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <input type="submit" value="Adicionar Produto" />
+        </form>
+      </div>
     </div>
   );
 }

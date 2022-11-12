e0754bd89d48f80d935078713a5840b0c1e2e205
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-11 21:20:01 -0300

    Ajustes finais

--- 06_SECRET_WORD/secretword/src/components/GameOver.js
+++ 06_SECRET_WORD/secretword/src/components/GameOver.js
@@ -2,12 +2,13 @@ import './GameOver.css'
 
 const GameOver = ({ retry, score }) => {
     return (
-        <div>
+        <div className="gameOver">
             <h1>Game Over</h1>
             <h2>
                 A sua pontuação foi: <span>{score}</span>
             </h2>
             <button onClick={retry}>Resetar jogo</button>
+            <p>Feito por Ricardo Rocker</p>
         </div>
     )
 }

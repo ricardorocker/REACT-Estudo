ab8da3270c9bb1052be98472ed94efe950418bdf
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-15 17:14:50 -0300

    Adicionando links

--- 08_ROUTER_REACT/routerreact/src/App.js
+++ 08_ROUTER_REACT/routerreact/src/App.js
@@ -7,17 +7,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 
+// Components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
         </Routes>
       </BrowserRouter>
-
     </div>
   );
 }

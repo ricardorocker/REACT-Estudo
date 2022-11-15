0a924a837f5549a2d241a38dfa29233addcf9461
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-15 18:30:07 -0300

    Carregamento dinamico de dados

--- 08_ROUTER_REACT/routerreact/src/pages/Product.js
+++ 08_ROUTER_REACT/routerreact/src/pages/Product.js
@@ -1,12 +1,26 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Product = () => {
-    // 4 - ROTA DINAMICA
+  // 4 - ROTA DINAMICA
   const { id } = useParams();
+
+  // 5 - CARREGAMENTO DADO INVIDUAL
+  const url = `http://localhost:3000/products/${id}`;
+  
+  const { data: product, error, loading } = useFetch(url);
 
   return (
     <>
       <p> Id do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando...</p>}
+      {product && (
+        <div>
+          <h2>{product.name}</h2>
+          <p>R$ {product.price}</p>
+        </div>
+      )}
     </>
   );
 };

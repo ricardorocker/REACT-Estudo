b41f6ec0b367787304c97bac25dea6b3c68be5a5
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-15 19:04:40 -0300

    Link ativo - NavLink

--- 08_ROUTER_REACT/routerreact/src/components/Navbar.js
+++ 08_ROUTER_REACT/routerreact/src/components/Navbar.js
@@ -1,12 +1,19 @@
 //  2 - LINKS COM REACT ROUTER
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      <NavLink
+        to="/"
+        // className={({ isActive }) => (isActive ? "esta-ativo" : "nao-ativo")}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   );
 };

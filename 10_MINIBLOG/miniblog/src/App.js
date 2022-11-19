83f1988cdd367cf97ea5aff1d215f3ae06e50a76
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-19 20:31:44 -0300

    Configurando o React Router

--- 10_MINIBLOG/miniblog/src/App.js
+++ 10_MINIBLOG/miniblog/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Miniblog</h1>
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }

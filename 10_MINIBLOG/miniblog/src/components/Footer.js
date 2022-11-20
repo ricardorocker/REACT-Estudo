18e9677ccf2bfe3d0e004b3a048e9380b1ad8a27
ricardo.rocker <ricardo.rocker@itixti.com.br>
2022-11-19 21:56:07 -0300

    CSS navbar e footer

--- 10_MINIBLOG/miniblog/src/components/Footer.js
+++ 10_MINIBLOG/miniblog/src/components/Footer.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Excreva sobre o que voce tem interesse!</h3>
       <p>Mini Blog Ricardo Rocker &copy; 2022</p>
     </footer>

5164e66243501beece0d0b1d4cc30eaae559dd53
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2022-12-13 12:31:07 -0300

    Estruturando dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,26 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
 const Dashboard = () => {
+  const posts = [];
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/post/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts...</p>
+        </div>
+      )}
     </div>
   );
 };
--- 10_MINIBLOG/miniblog/src/pages/Home/Home.js
+++ 10_MINIBLOG/miniblog/src/pages/Home/Home.js
@@ -43,7 +43,7 @@ const Home = () => {
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
-            <Link to="/posts/create" className="btn">
+            <Link to="/post/create" className="btn">
               Criar primeiro post
             </Link>
           </div>
